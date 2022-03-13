a6d201a0c3ec73ecfd7044f5d3bafbaf6e2fdfe7
Junhyunny <kang3966@naver.com>
2022-03-13 19:55:15 +0900

    Build jest configuration file 내용 변경

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        '\\.(png|svg|pdf|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
-        '\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+        '\\.(png|pdf|svg|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|styl|less|sass|scss|svg)$': 'identity-obj-proxy',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],

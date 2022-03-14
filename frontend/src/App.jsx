c3f1ef8fbdc788c8a84205840b7dc284181024a0
Junhyunny <kang3966@naver.com>
2022-03-15 01:38:08 +0900

    Add query selector input box and plus, minus button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 const debounce = (fn, timeout = 500) => {
     let timer = null
@@ -14,24 +15,42 @@ const debounce = (fn, timeout = 500) => {
 }
 
 function App() {
+    const increaseHandler = () => {
+        setQueryBoxes([...queryBoxes, { query: '' }])
+    }
 
-    const urlChangeHandler = debounce(({target}) => {
+    const decreaseHandler = (index) => {
+        queryBoxes.splice(index, 1)
+        setQueryBoxes([...queryBoxes])
+    }
+
+    const [queryBoxes, setQueryBoxes] = useState([{ query: '' }])
+
+    const urlChangeHandler = debounce(({ target }) => {
         const requestBody = {
             url: target.value,
         }
-        axios.post('/api/html-document', requestBody)
-            .then(({data}) => {
-                const element = document.createElement('html')
-                element.innerHTML = data
-                const responseBody = document.querySelector('#responseBody')
-                responseBody.innerHTML = element.querySelector('body').innerHTML
-            })
+        axios.post('/api/html-document', requestBody).then(({ data }) => {
+            const element = document.createElement('html')
+            const responseBody = document.querySelector('#responseBody')
+            element.innerHTML = data
+            responseBody.innerHTML = element.querySelector('body').innerHTML
+        })
     })
 
     return (
         <div>
-            <input placeholder="put url what you want to crawl" onChange={urlChangeHandler}/>
-            <div id="responseBody" style={{width: 0, height: 0, overflow: 'hidden'}}></div>
+            <div id="responseBody" style={{ width: 0, height: 0, overflow: 'hidden' }}></div>
+            <input type="text" placeholder="put url what you want to crawl" onChange={urlChangeHandler} />
+            <a onClick={increaseHandler}>+</a>
+            {queryBoxes.map((queryBox, index) => {
+                return (
+                    <div key={index}>
+                        <input type="text" placeholder="query" defaultValue={queryBox.query} />
+                        <a onClick={() => decreaseHandler(index)}>-</a>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,19 +1,23 @@
-import {render, screen, waitFor} from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import App from './App'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
 
 describe('App', () => {
-    it('renders url input box', () => {
-        render(<App/>)
+    beforeEach(() => {
+        jest.restoreAllMocks()
+    })
+
+    it('renders url box', () => {
+        render(<App />)
 
         expect(screen.getByPlaceholderText('put url what you want to crawl')).toBeInTheDocument()
     })
 
-    it('fetches HTML document when type url into input box', () => {
+    it('fetches HTML document when type url into url box', () => {
         jest.useFakeTimers()
 
-        render(<App/>)
+        render(<App />)
         const spyAxios = jest.spyOn(axios, 'post')
         const urlBox = screen.getByPlaceholderText('put url what you want to crawl')
 
@@ -26,12 +30,22 @@ describe('App', () => {
         })
     })
 
+    it('do not fetch HTML document with empty typed url box', () => {
+        jest.useFakeTimers()
+
+        render(<App />)
+        const spyAxios = jest.spyOn(axios, 'post')
+        jest.advanceTimersByTime(500)
+
+        expect(spyAxios).toHaveBeenCalledTimes(0)
+    })
+
     it('renders HTML document when fetches data', async () => {
         jest.useFakeTimers()
 
-        render(<App/>)
+        render(<App />)
         jest.spyOn(axios, 'post').mockResolvedValue({
-            data: `<body>Hello world<div class="data">Data</div></body>`
+            data: `<body>Hello world<div class="data">Data</div></body>`,
         })
 
         const urlBox = screen.getByPlaceholderText('put url what you want to crawl')
@@ -43,5 +57,31 @@ describe('App', () => {
             expect(screen.getByText('Hello world')).toBeInTheDocument()
             expect(screen.getByText('Data')).toBeInTheDocument()
         })
+    })
+
+    it('renders "query" input box, "+" button and "-" button', () => {
+        render(<App />)
+
+        expect(screen.getByPlaceholderText('query')).toBeInTheDocument()
+        expect(screen.getByText('+')).toBeInTheDocument()
+        expect(screen.getByText('-')).toBeInTheDocument()
+    })
+
+    it('click "+" button then increase "query" input box, "+" button and "-" button', () => {
+        render(<App />)
+        const plusButton = screen.getByText('+')
+        userEvent.click(plusButton)
+
+        expect(screen.getAllByPlaceholderText('query')).toHaveLength(2)
+        expect(screen.getAllByText('-')).toHaveLength(2)
+    })
+
+    it('click "-" button then decrease "query" input boxes, "+" buttons and "-" buttons', () => {
+        render(<App />)
+        const minusButton = screen.getByText('-')
+        userEvent.click(minusButton)
+
+        expect(screen.queryAllByPlaceholderText('query')).toHaveLength(0)
+        expect(screen.queryAllByText('-')).toHaveLength(0)
     })
 })

038bbaeaa9c7c027a36908a9d511955fd50c61ae
Junhyunny <kang3966@naver.com>
2022-03-24 05:06:08 +0900

    Test ExtractQueryUtil
    - 이벤트 추가
    - 쿼리 추출
    - 이벤트 제거

--- frontend/src/utils/ExtractQueryUtil.js
+++ frontend/src/utils/ExtractQueryUtil.js
@@ -1,5 +1,36 @@
-export const extractQueryFrom = (tagName) => {}
+let query = ''
 
-export default {
-    extractQueryFrom,
+const innerExtractQueryEvent = ({ currentTarget }) => {
+    if (query !== '') {
+        query += ' '
+    }
+    query += currentTarget.tagName
+    const classList = currentTarget.classList
+    for (let index = 0; index < classList.length; index++) {
+        query += '.' + classList[index]
+    }
+}
+
+export const addExtractQueryEventFrom = (targetId, tagName = 'div', exceptedIds = []) => {
+    const elements = document.getElementById(targetId).querySelectorAll(tagName)
+    Array.prototype.filter
+        .call(elements, (element) => !exceptedIds.includes(element.id))
+        .forEach((element) => {
+            element.addEventListener('click', innerExtractQueryEvent, true)
+        })
+}
+
+export const removeExtractQueryEventFrom = (targetId, tagName = 'div', exceptedIds = []) => {
+    const elements = document.getElementById(targetId).querySelectorAll(tagName)
+    Array.prototype.filter
+        .call(elements, (element) => !exceptedIds.includes(element.id))
+        .forEach((element) => {
+            element.removeEventListener('click', innerExtractQueryEvent, true)
+        })
+}
+
+export const extractQuery = () => {
+    const temp = query
+    query = ''
+    return temp
 }
--- frontend/src/utils/ExtractQueryUtil.test.js
+++ frontend/src/utils/ExtractQueryUtil.test.js
@@ -1,1 +1,41 @@
-describe('ExtractQueryUtil', () => {})
+import { render, screen } from '@testing-library/react'
+import { addExtractQueryEventFrom, extractQuery, removeExtractQueryEventFrom } from './ExtractQueryUtil'
+import userEvent from '@testing-library/user-event'
+
+describe('ExtractQueryUtil', () => {
+    it('extract query string from child div', () => {
+        render(
+            <div id="target">
+                <div className={'hello'}>hello</div>
+                <div className={'world'}>world</div>
+                <div className={'parent'}>
+                    <div className={'child'}>child</div>
+                </div>
+            </div>
+        )
+        addExtractQueryEventFrom('target', 'div', ['target'])
+
+        userEvent.click(screen.getByText('child'))
+
+        expect(extractQuery()).toEqual('DIV.parent DIV.child')
+        expect(extractQuery()).toEqual('')
+    })
+
+    it('could not extract query string from child div after remove event handler', () => {
+        render(
+            <div id="target">
+                <div className={'hello'}>hello</div>
+                <div className={'world'}>world</div>
+                <div className={'parent'}>
+                    <div className={'child'}>child</div>
+                </div>
+            </div>
+        )
+        addExtractQueryEventFrom('target', 'div', ['target'])
+        removeExtractQueryEventFrom('target', 'div', ['target'])
+
+        userEvent.click(screen.getByText('child'))
+
+        expect(extractQuery()).toEqual('')
+    })
+})
